4b134b659f10a595cc0436e89fd7d880342effd4
13707378528 <898758277@qq.com>
2024-06-23 21:11:00 +0800

    update

---
+++ parent/src/main/java/com/user/test/TestMain.java
@@ -0,0 +1,7 @@
+package com.user.test;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("快速入门。hellworld");
+    }
+}
---
+++ parent/src/main/java/com/user/test/TestUpdate.java
@@ -0,0 +1,7 @@
+package com.user.test;
+
+public class TestUpdate {
+    public static void main(String[] args) {
+        System.out.println("生活就是这样，不会坏到哪里去，也不会好到哪里去");
+    }
+}
